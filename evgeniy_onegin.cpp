b7ca4b5d72fedd0f107d06b0c93c22720149e739
maltoza <anny.v.moon@gmail.com>
2026-09-22 21:57:31 +0300

    функция переворачивания строки

--- evgeniy_onegin.cpp
+++ evgeniy_onegin.cpp
@@ -28,10 +28,7 @@ enum ERRORS
 
 //поменять порядок
 ERRORS read_from_file(char* (*ind_main)[MAX_NUM_LINES], size_t* num_lines);
-ERRORS write_to_file (const char* const (*ind)[MAX_NUM_LINES], const size_t num_lines, bool flag);
-bool is_alphas_in(const char* str);
-int free_values(char* ind[], const size_t num_lines);
-int free_ptrs(char* ind[], const size_t num_lines);
+
 ERRORS bubble_sort(void* array, const size_t num_elems, const size_t size_el, int (*comp)(void** prev_num, void** next_num));
 int compare_char_up(void** prev_str, void** next_str);
 int compare_char_down(void** prev_str, void** next_str);
@@ -39,6 +36,12 @@ char* reverse(char** str);
 int strcmp_my(char* str1, char* str2);
 char* eat_not_symb(char** str);
 ERRORS swap(void** str1, void** str2);
+
+ERRORS write_to_file (const char* const (*ind)[MAX_NUM_LINES], const size_t num_lines, bool flag);
+bool is_alphas_in(const char* str);
+
+int free_values(char* ind[], const size_t num_lines);
+int free_ptrs(char* ind[], const size_t num_lines);
 
 
 int main()
@@ -140,6 +143,126 @@ ERRORS read_from_file(char* (*ind_main)[MAX_NUM_LINES], size_t* num_lines)
     return ERRORS_OK;
 }
 
+
+// сортировка пузырьком
+ERRORS bubble_sort(void* array, const size_t num_elems, const size_t size_el, int (*comp)(void** prev_num, void** next_num))
+{
+    // проверка входных параметров
+    assert(array != NULL);
+    assert(comp != NULL);
+
+    // сортировка
+    for (size_t n = 0; n < num_elems; n++)
+    {
+        size_t num_swaps = 0;
+        for (size_t i = 0; i < num_elems - n - 1; i++)
+        {
+            num_swaps += comp((void**)((uintptr_t)array + i * size_el),
+                              (void**)((uintptr_t)array + (i + 1) * size_el));
+        }
+        if (num_swaps == 0)
+        {
+            break;
+        }
+    }
+
+    return ERRORS_OK;
+}
+
+
+// обмен строк по условию
+int compare_char_up(void** prev_str, void** next_str)
+{
+    if (strcmp_my(*(char**)prev_str, *(char**)next_str) > 0)
+    {
+        swap(prev_str, next_str);
+        return 1;
+    }
+
+    return 0;
+}
+
+int compare_char_down(void** prev_str, void** next_str)
+{
+    if (strcmp_my(reverse((char**)prev_str), reverse((char**)next_str)) > 0)
+    {
+        swap(prev_str, next_str);
+        return 1;
+    }
+
+    return 0; 
+}
+
+
+char* reverse(char** str)
+{
+    int len_local = strlen(*str) - 1;
+    int i = 0;
+
+    while (i < len_local)
+    {
+        char temp = (*str)[i];
+        (*str)[i] = (*str)[len_local];
+        (*str)[len_local] = temp;
+        i++;
+        len_local--;
+    }
+
+    return *str;
+}
+
+// сравнение строк
+int strcmp_my(char* str1, char* str2)
+{
+    // проверка входных параметров
+    assert(str1 != NULL);
+    assert(str2 != NULL);
+
+    // перебор символов
+    while(str1 != '\0' && str2 != '\0')
+    {
+        eat_not_symb(&str1);
+        eat_not_symb(&str2);
+        if (tolower(*str1) != tolower(*str2))
+        {
+            return tolower(*str1) - tolower(*str2);
+        }
+        str1++;
+        str2++;
+    }
+
+    return 0;
+}
+
+
+// устранение не букв
+char* eat_not_symb(char** str)
+{
+    while(!isalpha(**str) && ((**str) != '\0'))
+    {
+        (*str)++;
+    }
+
+    return *str;
+}
+
+// обмен указателей на строки
+ERRORS swap(void** str1, void** str2)
+{
+    // проверка входных параметров
+    assert(str1 != NULL);
+    assert(str2 != NULL);
+    
+    //обмен
+    void* temp = *str1;
+    *str1 = *str2;
+    *str2 = temp;
+
+    return ERRORS_OK;
+}
+
+
+
 //запись текста в файл
 ERRORS write_to_file(const char* const (*ind)[MAX_NUM_LINES], const size_t num_lines, bool flag)
 {
@@ -222,117 +345,3 @@ int free_ptrs(char* ind[], const size_t num_lines)
 }
 
 
-// сортировка пузырьком
-ERRORS bubble_sort(void* array, const size_t num_elems, const size_t size_el, int (*comp)(void** prev_num, void** next_num))
-{
-    // проверка входных параметров
-    assert(array != NULL);
-    assert(comp != NULL);
-
-    // сортировка
-    for (size_t n = 0; n < num_elems; n++)
-    {
-        size_t num_swaps = 0;
-        for (size_t i = 0; i < num_elems - n - 1; i++)
-        {
-            num_swaps += comp((void**)((uintptr_t)array + i * size_el),
-                              (void**)((uintptr_t)array + (i + 1) * size_el));
-        }
-        if (num_swaps == 0)
-        {
-            break;
-        }
-    }
-
-    return ERRORS_OK;
-}
-
-
-// обмен строк по условию
-int compare_char_up(void** prev_str, void** next_str)
-{
-    if (strcmp_my(*(char**)prev_str, *(char**)next_str) > 0)
-    {
-        swap(prev_str, next_str);
-        return 1;
-    }
-
-    return 0;
-}
-
-int compare_char_down(void** prev_str, void** next_str)
-{
-    if (strcmp_my(reverse((char**)(prev_str)), reverse((char**)(next_str))) > 0)
-    {
-        swap(prev_str, next_str);
-        return 1;
-    }
-
-    return 0; 
-}
-
-char* reverse(char** str)
-{
-    assert(str != NULL);
-
-    size_t len_str = strlen(*str) - 1;
-
-    for (size_t i = 0; i < len_str; i++)
-    {
-        char temp = (*str)[i];
-        (*str)[i] = (*str)[len_str - i - 1];
-        (*str)[len_str - i - 1] = temp;
-    }
-
-    return *str;
-}
-
-// сравнение строк
-int strcmp_my(char* str1, char* str2)
-{
-    // проверка входных параметров
-    assert(str1 != NULL);
-    assert(str2 != NULL);
-
-    // перебор символов
-    while(str1 != '\0' && str2 != '\0')
-    {
-        eat_not_symb(&str1);
-        eat_not_symb(&str2);
-        if (tolower(*str1) != tolower(*str2))
-        {
-            return tolower(*str1) - tolower(*str2);
-        }
-        str1++;
-        str2++;
-    }
-
-    return 0;
-}
-
-
-// устранение не букв
-char* eat_not_symb(char** str)
-{
-    while(!isalpha(**str) && ((**str) != '\0'))
-    {
-        (*str)++;
-    }
-
-    return *str;
-}
-
-// обмен указателей на строки
-ERRORS swap(void** str1, void** str2)
-{
-    // проверка входных параметров
-    assert(str1 != NULL);
-    assert(str2 != NULL);
-    
-    //обмен
-    void* temp = *str1;
-    *str1 = *str2;
-    *str2 = temp;
-
-    return ERRORS_OK;
-}
